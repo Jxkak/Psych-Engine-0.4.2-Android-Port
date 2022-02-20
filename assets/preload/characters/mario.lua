31b8ad2c98ab597df71fc754f9fa06a3003ee826
Jxkak <100052532+Jxkak@users.noreply.github.com>
2022-02-19 21:30:09 -0300

    Add files via upload

---
+++ assets/preload/characters/mario.lua
@@ -0,0 +1,8 @@
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+	health = getProperty('health')
+	if dadName == 'mario' then
+		if getProperty('health') > 0.02 then
+			setProperty('health', health- 0.015);
+		end
+	end
+end
